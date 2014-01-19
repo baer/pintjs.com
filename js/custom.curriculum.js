ad620b4f8baaf9bed1b70332c3df88ec68ae4d2e
Eric Baer <me@ericbaer.com>
2014-01-19 14:45:15 -0800

    New Site Skeleton

---
+++ js/custom.curriculum.js
@@ -0,0 +1,29 @@
+jQuery( document ).ready( function( $ ){
+
+	// Intro size setup
+
+	setupIntro(); // On ready, resize intro
+
+	$( window ).resize( function() { setupIntro(); } ); // On resize
+
+	function setupIntro() {
+		var infoBox = $('.info' );
+		var titleBox = $( '.info').find('.container');
+		$( '.info' ).find( '.container' ).css( 'top', ( ( infoBox.height() - titleBox.height() - 20 ) / 2) + 'px' );
+	}
+
+	// Making navigation sticky on scroll
+
+	$( '#nav' ).sticky( { topSpacing:10 } );
+
+	$( '#nav ul' ).onePageNav( { scrollSpeed: 400 } );
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() < $(window).height()/2) {
+			$('#nav').find('li').removeClass('current');
+		}
+	});
+
+	// Full background image
+	$( '.fx-backstretch' ).find( '.info' ).backstretch( 'img/backstretch.jpg' ); // Replace backstrech.jpg with your own image if needed
+});
